f7fbe02eff597dc32e7e0cd6eb4a34403cf67b85
Dhruv <pythonbrilliant@gmail.com>
2017-07-15 23:50:39 +0530

    Updated Greet Messages

--- modules/greet.py
+++ modules/greet.py
@@ -3,6 +3,7 @@ Hey
 I am Friday, a Smart Bot on Telegram.
 
 What I can do...
+
 /joke for a random joke
 /meme for a random meme
 /flip for flipping a coin
@@ -10,7 +11,9 @@ What I can do...
 /shorten URL for shortening a URL
 /expand SHORTURL for expanding a URL
 /wiki KEYWORD for a searching keyword
+/news Sends you latest news articles
 /song SONGNAME for sending song to you
+/video VIDEONAME for sending Youtube Video to you with that name
 /ytd LINK for downloading youtube video video
 /rate for Rating Our Bot
 /contact MESSAGE to send message to Bot Master
@@ -19,6 +22,9 @@ Wanna Report a Bug or send suggestions. Type
 /bug DESCRIPTION to report bug.
 /suggest SUGGESTION to suggest anything.
 
+
+Wanna Contribute ?
+Github :- http://github.com/dhruvramdev/friday-bot
 """
 
 hello_msg = """
